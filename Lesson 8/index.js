7c63d6662a874b042d5124f4a7851db5d3531ad4
Dmitriy Koltsov <koltsov.dmitriy.v@gmail.com>
2023-05-04 19:07:56 +0300

    polindrom End

---
+++ Lesson 8/index.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////Polindrom///////////////////////////
+//Way 1
+const polindrom = (string) => {
+    let reversedString = '';
+
+    for (let i = string.length - 1; i >= 0; i--){
+        reversedString += string[i];
+    };
+    console.log(reversedString);
+    return string.toLowerCase() === reversedString.toLowerCase();
+};
+console.log(polindrom('Lol'));
+//Way 2
+const polindromAnalog = (string) =>{
+    const reversedString = string.split('').reverse().join('');
+    console.log(reversedString);
+    return string.toLowerCase() === reversedString.toLowerCase();
+}
+console.log(polindromAnalog('LoL 123 321 lol'));

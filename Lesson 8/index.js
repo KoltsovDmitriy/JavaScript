ff946f9d15de4bc1ea9327e5977ac64e5edb5a5d
Dmitriy Koltsov <koltsov.dmitriy.v@gmail.com>
2023-05-05 03:00:49 +0300

    getWord End

--- Lesson 8/index.js
+++ Lesson 8/index.js
@@ -1,30 +1,56 @@
-// Задача: изменять окончание слова в зависимости от количества.
-//Русккие буквы.
+//Russian language.
 
 const getWord = (number,arrayWords) => {
     const division = number % 10;
 
-    if (division == 1 && number != 11){
+    if (division == 1 && number != 11) {
         console.log(number, arrayWords[0]);
     }
     else
-    if((division == 2 && number != 12) || (division == 3 && number != 13) || (division == 4 && number != 14)){
+    if((division == 2 && number != 12) || (division == 3 && number != 13) || (division == 4 && number != 14)) {
         console.log(number, arrayWords[1]);       
     }
     else
         console.log(number, arrayWords[2]);
 };
 
-getWord(72, ['фильм', 'фильма', 'фильмов']);
+getWord(76, ['фильм', 'фильма', 'фильмов']);
 getWord(736, ['предмет', 'предмета', 'предметов']);
 
 
-//Английский язык. Алгорить взял https://skysmart.ru/articles/english/mnozhestvennoe-chislo-sushestvitelnyh-v-anglijskom-yazyke
+//English language. Logic from https://skysmart.ru/articles/english/mnozhestvennoe-chislo-sushestvitelnyh-v-anglijskom-yazyke
 
-const getWordEng= (number,word) => {
-
+const getWordEng= (number, string) => {
+    const word = string.toLowerCase();
+    const endingOneLetter = word.slice(word.length - 1);
+    const endingTwoLetter = word.slice(word.length - 2);
+    const exceptions = 'man, men, person, people, woman, women, mouse, mice, foot, feet, child, children, tooth, teeth, goose, geese, ox, oxen, bacterium, bacteria, hypothesis, hypotheses, phenomenon, phenomena, stimulus, stimuli, fish, fish, fruit, fruit, species, species, deer, deer, sheep, sheep, water, water, dirt, dirt, cream,cream, love, love, information, information';
+    const exceptionsArray = exceptions.split(', ');
+   
+    if (number == 1){
+        console.log(number, word);
+    }
+    else if(['s', 'x', 'z', 'o'].includes(endingOneLetter) || ['sh', 'ch', 'ss'].includes(endingTwoLetter)) {
+        console.log(number, word +'es');
+    } 
+    else if (['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'].includes(endingTwoLetter[0]) && (endingOneLetter == 'y')) {
+        console.log(number, word.slice(0, -1) +'ies');       
+    }
+    
+    else if(endingOneLetter == 'f'){
+        console.log(number, word.slice(0, -1) +'ves');
+    }
+    else if(endingTwoLetter == 'fe')
+    console.log(number, word.slice(0, -2) +'ves');
+    else if(exceptionsArray.includes(word)){
+        console.log(number, exceptionsArray[exceptionsArray.indexOf(word)+1]);
+    }
+    else
+    console.log(number, word + 's')
+   
+    
 }
-getWordEng(5, 'apple')
+getWordEng(2, 'cat')
 
 
 

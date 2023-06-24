969e899014f171ff2c5de2574e30ebd712e438a0
Dmitriy Koltsov <koltsov.dmitriy.v@gmail.com>
2023-06-24 05:05:21 +0300

    modify

--- Lesson 13/index.js
+++ Lesson 13/index.js
@@ -1,42 +1,43 @@
 import {data} from './data.js';
+///1
+
 
 ///2
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("burger").addEventListener("click", () =>
-    {
-        document.querySelector(".header").classList.toggle("open")
-    })
-})
+document.getElementById("burger").addEventListener("click", () =>
+{
+    document.querySelector(".header").classList.toggle("open")
+});
+
 
 ///3
-const openSignIn = document.querySelector('.header__link5');
-const closeSingIn = document.querySelector('.cross');
 const form = document.querySelector('.sign-up__wrapper');
 
-const openCloseForms = (divForm, state) => {
-    divForm.addEventListener('click', () => {
-        form.style.visibility = state;        
-    })
+const openCloseForms = (className, condition) => {
+    document.querySelector(className).addEventListener('click', () => {
+    form.classList.toggle('open', condition);
+    })   
 }
-openCloseForms(openSignIn, "visible");
-openCloseForms(closeSingIn, "hidden");
+
+openCloseForms('.header__link--btn', true);
+openCloseForms('.cross', false);
+
 
 ///4
-const links = document.querySelectorAll('.link');
+const links = document.querySelectorAll('.menu__link');
 const handleLink = (e) => {
-    e.preventDefault();
     const { currentTarget } = e;
     const href = currentTarget.getAttribute("href");
-    if(!href) return;
-    const element = document.querySelector(href);
-    if(!element) return;
-    const top = element.offsetTop; 
-    window.scrollTo({ behavior: "smooth", top})
+    if(!href || !href.startsWith('#') || href.length < 2) return;
+        e.preventDefault();
+        const element = document.querySelector(href);
+        if(!element) return;
+        const top = element.offsetTop; 
+        window.scrollTo({ behavior: "smooth", top})
 }
-
 links.forEach(link => {
     link.addEventListener('click', handleLink)
 });
+
 
 ///5
 const sliderBackground = document.querySelector('.slider__image');
@@ -44,137 +45,95 @@ const sliderText = document.querySelector('.slider__title');
 
     let i = 1;
     setInterval(()=>{
-        ++i;
-        if (i === 4) {
-        i = 1;
-        }
+       i = i > 2 ? 1 : i + 1;
         sliderBackground.style.backgroundImage = `url(./img/slider_image${i}.png`;
         sliderText.style.backgroundImage = `url(./img/slider_image${i}_${i}.png`;
     }, 4000);
+
 
 ///6
 const typeNames = data.map((type) => {
     return type.type
 });
-const allTypes = [... new Set(typeNames.flat())].reverse();
+const allTypes = [... new Set(typeNames.flat())];
 
-const collectionOfTypes = allTypes.map((type) => {
-    const moviesArray = [];
-    data.map((movie) => {        
-        if ((movie.type).includes(type)){    
-            moviesArray.push(movie);
-        }; 
-    });
-    const collection = {type, moviesArray};
-    return collection;    
+const getItemTemplate = (item) => {
+    return `<div class="film" style="background-image: url(${item.image});">
+                <div class="hover">
+                    <div class="film__flex">                                
+                        <img class="film__info--toggle" src="./img/Hover/Toggle.png" alt="Toggle">
+                        <div class="film__info--rating">${item.rating}</div>
+                        <div class="film__info--year">${item.year}</div>
+                    </div>
+                    <div class="film__name">${item.title}</div>
+                    <div class="film__description">${item.description}</div>
+                    <div class="film__button">
+                        <div><img src="./img/Hover/Icon_watch.png" class="film__button--text" alt=""></div>
+                        <div><a href="${item.trailer}" class="film__button--watch">WATCH</a></div>
+                    </div>
+                </div>
+            </div>`;
+}   
+
+const setItemTemplate = (type) => {
+    data.forEach((card) => {
+        if (card.type === type) {
+        const element = document.querySelector(`.${type}`);
+        element.insertAdjacentHTML('beforeend', getItemTemplate(card));
+        }
+    })
+}
+
+const setItems = allTypes.forEach((type) =>{
+    setItemTemplate(type);
 })
-// console.log(collectionOfTypes);
 
-// const getTemplateCards = (data) => {
-//     data.forEach((cards) => {        
-//         const card = `<div class="film" style="background-image: url(${cards.image});">
-//                 <div class="hover">
-//                     <div class="film__flex">                                
-//                         <img class="film__info--toggle" src="./img/Hover/Toggle.png" alt="Toggle">
-//                         <div class="film__info--rating">${cards.rating}</div>
-//                         <div class="film__info--year">${cards.year}</div>
-//                     </div>
-//                     <div class="film__name"><p>${cards.title}</p></div>
-//                     <div class="film__description"><p>${cards.description}</p></div>
-//                     <div class="film__button">
-//                         <a href="${cards.trailer}" class="film__button--watch">WATCH</a>
-//                         <img src="./img/Hover/Icon_watch.png" class="film__button--text" alt="">
-//                     </div>
-//                 </div>
-//             </div>`;
-//             return cards;
-//     })
-// }
-// const getMovie = ()=>{
-//     const getMovieCard = data.forEach(movie => {
-//             if(movie.type.includes('show')) {
-//             const container = document.querySelector('.shows');
-//             container.insertAdjacentHTML('beforeend', getTemplateCards)
-           
+
+//7
+const openDiv = document.querySelectorAll('.question');
+openDiv.forEach((event) => {
+    event.addEventListener('click', () => {
+        const parent = event.parentNode;
+        (parent.classList.contains('open')) ? (parent.classList.remove('open')) : 
+        (document.querySelectorAll('.faq').forEach((child) => {child.classList.remove('open')}),
+        parent.classList.add('open'))
+    })
+})
+
+
+
+
+// Дичь
+// document.getElementById("question").addEventListener("click", () =>
+// {
+//     document.querySelector(".faq__answer").classList.toggle("open");
+//     document.querySelector(".faq").classList.toggle("open");
+// });
+// const div = document.querySelectorAll('.question');
+// div.forEach((divItem) => {
+//     divItem.addEventListener("click", (e) => {
+//         document.querySelectorAll(".faq__answer, .faq").forEach((className) => className.classList.toggle('open'))});
+// })
+// const div = document.querySelector('.question');
+// div.addEventListener('click', (e) => {
+//     document.querySelector(".faq__answer").classList.toggle("open");
+// })
+// const btnQuestion = document.querySelectorAll('.question');
+// const divAnswer = document.querySelectorAll('.faq__answer');
+// btnQuestion.forEach((link)=>{
+//     link.addEventListener('click',() =>{
+//         divAnswer.forEach((answer)=>{
+//             const handler = () =>{
+//                 answer.classList.remove('faq-enter-active');
+//                 answer.removeEventListener('transitionend', handler);
 //             }
-//         });
-//     }
-// getMovie();
-
-// const getTemplateCards = (data) => {
-//     data.forEach((cards) => {        
-//         const card = `<div class="film" style="background-image: url(${cards.image});">
-//                 <div class="hover">
-//                     <div class="film__flex">                                
-//                         <img class="film__info--toggle" src="./img/Hover/Toggle.png" alt="Toggle">
-//                         <div class="film__info--rating">${cards.rating}</div>
-//                         <div class="film__info--year">${cards.year}</div>
-//                     </div>
-//                     <div class="film__name"><p>${cards.title}</p></div>
-//                     <div class="film__description"><p>${cards.description}</p></div>
-//                     <div class="film__button">
-//                         <a href="${cards.trailer}" class="film__button--watch">WATCH</a>
-//                         <img src="./img/Hover/Icon_watch.png" class="film__button--text" alt="">
-//                     </div>
-//                 </div>
-//             </div>`;
-//     })
-    
-// }
-// console.log(getTemplateCards)
-
-// const getTemplate = (type) => {
-//     const foundMovie = collectionOfTypes.find((movie) => movie.type = type)
-//     // console.log(foundMovie.moviesArray); 
-//         const [{moviesArray}] = collectionOfTypes;
-//         // console.log(moviesArray);
-//         if (foundMovie) return (
-//             moviesArray.map((sortMovie) => { 
-//             `<div class="film" style="background-image: url(${sortMovie.image});">
-//                 <div class="hover">
-//                     <div class="film__flex">                                
-//                         <img class="film__info--toggle" src="./img/Hover/Toggle.png" alt="Toggle">
-//                         <div class="film__info--rating">${sortMovie.rating}</div>
-//                         <div class="film__info--year">${sortMovie.year}</div>
-//                     </div>
-//                     <div class="film__name"><p>${sortMovie.title}</p></div>
-//                     <div class="film__description"><p>${sortMovie.description}</p></div>
-//                     <div class="film__button">
-//                         <a href="${sortMovie.trailer}" class="film__button--watch">WATCH</a>
-//                         <img src="./img/Hover/Icon_watch.png" class="film__button--text" alt="">
-//                     </div>
-//                 </div>
-//             </div>`
-//             return sortMovie;
+//             answer.style.display = 'block';
+//             answer.classList.add('faq-enter');
+//             setTimeout(() => {
+//                 answer.classList.add('faq-enter-active');
+//                 answer.classList.remove('faq-enter');
+//             }, 200);
+//             answer.addEventListener('transitionend', handler)
 //         })
-//     );
-   
-// }
-// getTemplate('show')
-// console.log(getTemplate());
-
-// const setItems = ()=>{
-//     const container = document.querySelector('.show');
-
-//     collectionOfTypes.forEach((show) => {
-//         container.insertAdjacentHTML('beforeend', getTemplate(show));
 //     })
-
-// }
-// setItems();
-
-// const setItems = ()=>{
-    
-//     allTypes.forEach((type) =>{
-        
-//         // console.log(type);
-//         if(type === 'show') {
-//         const container = document.querySelector('.show');
-//         // console.log(container);
-        
-//         container.insertAdjacentHTML('beforeend', getTemplateCards);
-        
-//         }
-//     })
-// }
-// setItems();
+// })
